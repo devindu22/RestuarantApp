4dff6acfb9f7cbeeca82b91e6818f08ea2747939
devindu malshan <devindu.nwcv@gmail.com>
2022-11-15 11:01:32 +0530

    navigation install

--- App.js
+++ App.js
@@ -1,28 +1,8 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import {View, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-function HomeScreen() {
+export default function App() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
   );
 }
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
